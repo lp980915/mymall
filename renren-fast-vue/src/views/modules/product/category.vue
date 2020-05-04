c9a8b043ec3e6a4bcfd1a38b3d863d3f7563ee2b
lp980915 <15596712191@163.com>
2020-05-04 21:40:33 +0800

    5.4

---
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "category",
+      created() {
+       this.getMenus();
+      },
+      data(){
+          return {
+
+          }
+      },
+      methods: {
+        handleNodeClick(data) {
+          console.log(data);
+        },
+        getMenus(){
+          this.$http({
+            url: this.$http.adornUrl('/product/category/list/tree'),
+            method: 'get',
+          }).then(res=>{
+            console.log(res);
+          })
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
