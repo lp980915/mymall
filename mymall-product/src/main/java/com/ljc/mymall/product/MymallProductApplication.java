ae73b07ea107818b1aea167737f51b5d874972ea
lp980915 <15596712191@163.com>
2020-05-05 11:53:48 +0800

    5.5

--- mymall-product/src/main/java/com/ljc/mymall/product/MymallProductApplication.java
+++ mymall-product/src/main/java/com/ljc/mymall/product/MymallProductApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
- * 整合mybatis-plus
+ * 逻辑删除
+ * 1.配置全局逻辑删除规则（可省略）
+ * 2.配置逻辑删除bean（新版本不需要了）
+ * 3.给实体类相应字段加上注解@TableLogic
  */
 @MapperScan("com.ljc.mymall.product.dao")
 @SpringBootApplication
--- renren-fast-vue/src/views/modules/product/category.vue
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -1,39 +1,64 @@
 <template>
-    <div>
-      <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
-    </div>
+  <div>
+    <el-tree :data="menus" :props="defaultProps" :expand-on-click-node="false" show-checkbox node-key="catId">
+        <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            v-if="node.level<=2"
+            type="text"
+            size="mini"
+            @click="() => append(data)">
+            Append
+          </el-button>
+          <!--无子节点才可以删除-->
+          <el-button
+            v-if="node.childNodes.length==0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)">
+            Delete
+          </el-button>
+        </span>
+        </span>
+    </el-tree>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "category",
-      created() {
-       this.getMenus();
-      },
-      data(){
-          return {
-            menus:[],
-            defaultProps: {
-              children: 'children',
-              label: 'name'
-            }
-          }
-      },
-      methods: {
-        handleNodeClick(data) {
-          console.log(data);
-        },
-        getMenus(){
-          this.$http({
-            url: this.$http.adornUrl('/product/category/list/tree'),
-            method: 'get',
-            //解构data
-          }).then(({data})=>{
-            this.menus=data.data
-          })
+  export default {
+    name: "category",
+    created() {
+      this.getMenus();
+    },
+    data() {
+      return {
+        menus: [],
+        defaultProps: {
+          children: 'children',
+          label: 'name'
         }
       }
+    },
+    methods: {
+      getMenus() {
+        this.$http({
+          url: this.$http.adornUrl('/product/category/list/tree'),
+          method: 'get',
+          //解构data
+        }).then(({data}) => {
+          this.menus = data.data
+        })
+      },
+      append(data) {
+        console.log("append", data);
+      },
+
+      remove(node, data) {
+        console.log("remove", node, data);
+      },
     }
+  }
 </script>
 
 <style scoped>
